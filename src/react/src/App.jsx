f85b16688433956c8111565ff4adb612ab6314da
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-08-12 19:20:13 -0300

    feat: adicionar rotas para as páginas existentes

--- src/react/src/App.jsx
+++ src/react/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Login from "./Views/Login/Login.jsx";
+import {Route, Routes} from "react-router-dom";
+import PaginaInicial from "./Views/PaginaInicial/PaginaInicial.jsx";
 import Cadastro from "./Views/Cadastro/Cadastro.jsx";
 
 function App() {
 
   return (
     <>
-        <Cadastro/>
+        <Routes>
+            <Route path="/" element={<PaginaInicial/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/cadastro" element={<Cadastro/>}/>
+        </Routes>
     </>
   )
 }
