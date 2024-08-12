e0c296070b03ab82042da18144b5eaa291984fcf
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-08-12 19:21:06 -0300

    feat: adicionar link para cadastro

--- src/react/src/Views/Login/Login.jsx
+++ src/react/src/Views/Login/Login.jsx
@@ -1,14 +1,16 @@
 import "./Login.css"
+import {Link} from "react-router-dom";
 
 export default function Login(){
     return (
     <div className="main">
         <form action="">
+            <Link to="/"><img className="logo-img" src="/logo.png" /></Link>
             <p className="title"> Login </p>
             <input className="mainshadowdown" type="email" placeholder="Email" required/>
             <input className="mainshadowdown" type="password" placeholder="Password" required/>
             <button  className="btn btn-block mainshadowdown">Login</button>
-            <p className="message">Not Registered?</p>
+            <p className="message">Não possui uma conta? <Link to="/cadastro">Cadastre-se</Link></p>
             </form>
     </div>
     )
