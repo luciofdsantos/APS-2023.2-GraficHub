3fb614a54fc51f6dd797c25a1c9bba591ce0b2ff
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-08-14 13:19:39 -0300

    refactor:change index to showLoginForm in AuthController

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function index()
+    public function showLoginForm()
     {
         return view('auth.login');
     }
--- src/routes/web.php
+++ src/routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () { return view('home');})->name('home');
 
 Route::controller(AuthController::class)->group(function () {
-    Route::get('/login', 'index')->name('auth.loginForm');
+    Route::get('/login', 'showLoginForm')->name('auth.loginForm');
     Route::post('/login', 'login')->name('auth.login');
     Route::get('/logout', 'logout')->name('auth.logout');
 });
