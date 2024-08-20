f4d5696fc338713afad2c57470b7a5a598189934
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-08-20 19:13:42 -0300

    style: centralizar verticalmente corrigindo erro de responsividade

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -12,31 +12,32 @@
     @if (auth()->check())
         Usuario logado, {{ auth()->user()->nome }} <a href="{{ route('auth.logout') }}"> Logout </a>
     @else
+        <div class="login-form-container">
+            <div class="main">
+                <form action="{{ route('auth.login') }}" method="post">
+                    <a href="{{ route('home')}}"> <img class="logo-img" alt="logo" src="/img/logo.png"/> </a>
+                    <p class="title"> Login </p>
+                    @csrf
+                    <input class="mainshadowdown" type="text"  placeholder="Email" name="email">
+                    @error('email')
+                    <span class="error-message">
+                   {{ $message }}
+                    </span>
+                    @enderror
+                    <input class="mainshadowdown" type="password" placeholder="Senha" name="password">
+                    @error('password')
+                    <span class="error-message">
+                    {{ $message }}
+                    </span>
+                    @enderror
+                    @error('error')
+                    <span class="error-message">{{ $message }}</span>
+                    @enderror
+                    <button class="mainshadowdown" type="submit">Login</button>
+                    <p class="message">Não possui uma conta? <a href="{{ route('user.create') }}"> Cadastre-se</a></p>
 
-        <div class="main">
-            <form action="{{ route('auth.login') }}" method="post">
-                <a href="{{ route('home')}}"> <img class="logo-img" alt="logo" src="/img/logo.png"/> </a>
-                <p class="title"> Login </p>
-                @csrf
-                <input class="mainshadowdown" type="text"  placeholder="Email" name="email">
-                @error('email')
-                <span class="error-message">
-               {{ $message }}
-                </span>
-                @enderror
-                <input class="mainshadowdown" type="password" placeholder="Senha" name="password">
-                @error('password')
-                <span class="error-message">
-                {{ $message }}
-                </span>
-                @enderror
-                @error('error')
-                <span class="error-message">{{ $message }}</span>
-                @enderror
-                <button class="mainshadowdown" type="submit">Login</button>
-                <p class="message">Não possui uma conta? <a href="{{ route('user.create') }}"> Cadastre-se</a></p>
-
-            </form>
+                </form>
+            </div>
         </div>
 
     @endif
--- src/resources/views/project/edit.blade.php
+++ src/resources/views/project/edit.blade.php
@@ -10,89 +10,90 @@
     <link href="/css/login.css" rel="stylesheet">
 </head>
 <body>
-
-<div class="main">
-    <form action="{{ route('project.update', $project->id) }}" method="post" enctype="multipart/form-data">
-        <a href="{{ route('home')}}"> <img class="logo-img" alt="logo" src="/img/logo.png"/> </a>
-        <p class="title"> Atualizar Dados</p>
-        @csrf
-        @method('PUT')
-        <input type="hidden" name="id" value="{{ $project->id }}">
-        <input type="text" placeholder="Título" name="titulo" value="{{ old('titulo', $project->titulo) }}">
-        @error('titulo')
-        <span>
-                {{$message}}
-            </span>
-        @enderror
-        <label class="custom-file-upload" for="cover-file-upload">Capa do projeto
-            <div id="cover-preview-wrapper"></div>
-        </label>
-        <input id="cover-file-upload" type="file" placeholder="Imagem da capa do projeto" name="imagem_capa">
-        @error('imagem_capa')
-        <span>
-                {{$message}}
-            </span>
-        @enderror
-        <label class="custom-file-upload" for="body-imgs-upload">Imagens do corpo do projeto
-            <div id="images-preview-wrapper"></div>
-        </label>
-        <input id="body-imgs-upload" type="file" name="imagens[]" multiple>
-        @error('imagens')
-        <span>
-                {{$message}}
-            </span>
-        @enderror
-        @error('imagens.*')
-        <span>
-                {{$message}}
-            </span>
-        @enderror
-        <input type="text" placeholder="Ferramentas" name="ferramentas"
-               value="{{ old('ferramentas', $project->ferramentas) }}">
-        @error('ferramentas')
-        <span>
-                {{$message}}
-            </span>
-        @enderror
-        <textarea class="textar" placeholder="Descricao"
-                  name="descricao">{{ old('descricao', $project->descricao) }}</textarea>
-        @error('descricao')
-        <span>
-                {{$message}}
-            </span>
-        @enderror
-        <input type="text" placeholder="Tags" name="tags" value="{{ old('tags', $project->tags) }}">
-        @error('tags')
-        <span>
-                {{$message}}
-            </span>
-        @enderror
-        <label class="custom-file-upload" for="file-upload">Arquivo do projeto</label>
-        <input id="file-upload" type="file" placeholder="Arquivo do projeto" name="arquivo">
-        @error('arquivo')
-        <span>
-                {{$message}}
-            </span>
-        @enderror
-        <div class="custom-check">
-            <p>Tornar arquivo privado?</p>
-            <div class="checkbox-wrapper-55">
-                <label class="rocker rocker-small">
-                    <input type="checkbox" name="arquivo_publico" {{ $project->arquivo_publico ? 'checked' : '' }} >
-                    <span class="switch-left">Yes</span>
-                    <span class="switch-right">No</span>
-                </label>
+<div class="project-form-container">
+    <div class="main">
+        <form action="{{ route('project.update', $project->id) }}" method="post" enctype="multipart/form-data">
+            <a href="{{ route('home')}}"> <img class="logo-img" alt="logo" src="/img/logo.png"/> </a>
+            <p class="title"> Atualizar Projeto</p>
+            @csrf
+            @method('PUT')
+            <input type="hidden" name="id" value="{{ $project->id }}">
+            <input type="text" placeholder="Título" name="titulo" value="{{ old('titulo', $project->titulo) }}">
+            @error('titulo')
+            <span>
+                    {{$message}}
+                </span>
+            @enderror
+            <label class="custom-file-upload" for="cover-file-upload">Capa do projeto
+                <div id="cover-preview-wrapper"></div>
+            </label>
+            <input id="cover-file-upload" type="file" placeholder="Imagem da capa do projeto" name="imagem_capa">
+            @error('imagem_capa')
+            <span>
+                    {{$message}}
+                </span>
+            @enderror
+            <label class="custom-file-upload" for="body-imgs-upload">Imagens do corpo do projeto
+                <div id="images-preview-wrapper"></div>
+            </label>
+            <input id="body-imgs-upload" type="file" name="imagens[]" multiple>
+            @error('imagens')
+            <span>
+                    {{$message}}
+                </span>
+            @enderror
+            @error('imagens.*')
+            <span>
+                    {{$message}}
+                </span>
+            @enderror
+            <input type="text" placeholder="Ferramentas" name="ferramentas"
+                   value="{{ old('ferramentas', $project->ferramentas) }}">
+            @error('ferramentas')
+            <span>
+                    {{$message}}
+                </span>
+            @enderror
+            <textarea class="textar" placeholder="Descricao"
+                      name="descricao">{{ old('descricao', $project->descricao) }}</textarea>
+            @error('descricao')
+            <span>
+                    {{$message}}
+                </span>
+            @enderror
+            <input type="text" placeholder="Tags" name="tags" value="{{ old('tags', $project->tags) }}">
+            @error('tags')
+            <span>
+                    {{$message}}
+                </span>
+            @enderror
+            <label class="custom-file-upload" for="file-upload">Arquivo do projeto</label>
+            <input id="file-upload" type="file" placeholder="Arquivo do projeto" name="arquivo">
+            @error('arquivo')
+            <span>
+                    {{$message}}
+                </span>
+            @enderror
+            <div class="custom-check">
+                <p>Tornar arquivo privado?</p>
+                <div class="checkbox-wrapper-55">
+                    <label class="rocker rocker-small">
+                        <input type="checkbox" name="arquivo_publico" {{ $project->arquivo_publico ? 'checked' : '' }} >
+                        <span class="switch-left">Yes</span>
+                        <span class="switch-right">No</span>
+                    </label>
+                </div>
             </div>
-        </div>
-        @error('arquivo_publico')
-        <span>
-                {{$message}}
-            </span>
-        @enderror
+            @error('arquivo_publico')
+            <span>
+                    {{$message}}
+                </span>
+            @enderror
 
-        <button type="submit">Atualizar</button>
+            <button type="submit">Atualizar</button>
 
-    </form>
+        </form>
+    </div>
 </div>
 <script>
     let img = document.getElementById('body-imgs-upload');
