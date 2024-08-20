a8121b5f3a97e1e5f889f68d12b6ddde1fa8ecf6
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-08-20 19:14:07 -0300

    feat: adicionar e remover imagens iterativamente

--- src/resources/views/project/create.blade.php
+++ src/resources/views/project/create.blade.php
@@ -11,9 +11,11 @@
 </head>
 <body>
 
+<div class="project-form-container">
     <div class="main">
         <form action="{{route('project.store')}}" method="post" enctype="multipart/form-data">
             <a href="{{ route('home')}}"> <img class="logo-img" alt="logo" src="/img/logo.png"/> </a>
+            <p class="title">Criar Projeto</p>
             @csrf
             <input type="text" placeholder="Título" name="titulo" value="{{ old('titulo') }}">
             @error('titulo')
@@ -28,7 +30,7 @@
                 {{$message}}
             </span>
             @enderror
-            <label  class ="custom-file-upload" for="body-imgs-upload">Imagens do corpo do projeto<div id="images-preview-wrapper"></div></label>
+            <label  class ="custom-file-upload" for="body-imgs-upload"><div id="body-imgs-upload-label-text">Imagens do corpo do projeto</div><div id="images-preview-wrapper"></div></label>
             <input id="body-imgs-upload" type="file" name="imagens[]" multiple>
             @error('imagens')
             <span>
@@ -58,7 +60,7 @@
                 {{$message}}
             </span>
             @enderror
-            <label class ="custom-file-upload" for="file-upload">Arquivo do projeto</label>
+            <label id="project-file-upload-label" class ="custom-file-project-upload" for="file-upload">Arquivo do projeto</label>
             <input id="file-upload" type="file" placeholder="Arquivo do projeto" name="arquivo">
             @error('arquivo')
             <span>
@@ -81,31 +83,99 @@
             </span>
             @enderror
 
-            <button type="submit">Criar projeto</button>
+            <button id="create-project-btn" type="submit">Criar projeto</button>
         </form>
     </div>
+</div>
     <script >
-        let img = document.getElementById('body-imgs-upload');
-        img.addEventListener("change", () =>{
-            let imgWrapper = document.getElementById("images-preview-wrapper");
+        let coverInput = document.getElementById('cover-file-upload');
+        coverInput.addEventListener("change", () =>{
+            let imgWrapper = document.getElementById("cover-preview-wrapper");
             imgWrapper.innerHTML = "<div>";
-            for(let i = 0; i< img.files.length; ++i){
-                let url = URL.createObjectURL(img.files[i])
+            for(let i = 0; i< coverInput.files.length; ++i){
+                let url = URL.createObjectURL(coverInput.files[i])
                 imgWrapper.innerHTML += `<img class="img-preview" src="${url}" alt="image${i}Preview">`
             }
             imgWrapper.innerHTML += "</div>"
         } )
 
-        let cover = document.getElementById('cover-file-upload');
-        cover.addEventListener("change", () =>{
-            let imgWrapper = document.getElementById("cover-preview-wrapper");
-            imgWrapper.innerHTML = "<div>";
-            for(let i = 0; i< cover.files.length; ++i){
-                let url = URL.createObjectURL(cover.files[i])
-                imgWrapper.innerHTML += `<img class="img-preview" src="${url}" alt="image${i}Preview">`
+        let imgsInput = document.getElementById('body-imgs-upload');
+        let files = [];
+        imgsInput.addEventListener("change", () =>{
+            for(let i = 0; i< imgsInput.files.length; ++i){
+                files.push(imgsInput.files[i]);
             }
-            imgWrapper.innerHTML += "</div>"
+            let dataTransfer = new DataTransfer();
+            for(let i = 0; i< files.length; ++i){
+                dataTransfer.items.add(files[i]);
+            }
+            imgsInput.files = dataTransfer.files;
+
+            let imgWrapper = document.getElementById("images-preview-wrapper");
+            if(imgsInput.files.length > 0) {
+                imgWrapper.innerHTML = "<div class='imgs-preview-wrapper'>";
+                for (let i = 0; i < imgsInput.files.length; ++i) {
+                    let url = URL.createObjectURL(imgsInput.files[i])
+                    imgWrapper.innerHTML += `<img class="img-preview" id="image-preview-${i}" src="${url}" alt="image${i}Preview" onclick="removePic(this, event)" onmouseover="showPicDeleteMessage()" onmouseout="hidePicDeleteMessage()">`
+                }
+                imgWrapper.innerHTML += "</div>"
+            }
         } )
+
+        function removePic(imgPreview, event){
+            event.preventDefault();
+            let i = Number(imgPreview.id.toString().slice(14));
+            files = files.filter((file, index) => index !== i)
+
+            let dataTransfer = new DataTransfer();
+            for(let i = 0; i< files.length; ++i){
+                dataTransfer.items.add(files[i]);
+            }
+            imgsInput.files = dataTransfer.files;
+            let imgWrapper = document.getElementById("images-preview-wrapper");
+            if(imgsInput.files.length > 0) {
+                imgWrapper.innerHTML = "<div>";
+                for (let i = 0; i < imgsInput.files.length; ++i) {
+                    let url = URL.createObjectURL(imgsInput.files[i])
+                    imgWrapper.innerHTML += `<img class="img-preview" id="image-preview-${i}" src="${url}" alt="image${i}Preview" onclick="removePic(this, event)" onmouseover="showPicDeleteMessage()" onmouseout="hidePicDeleteMessage()">`
+                }
+                imgWrapper.innerHTML += "</div>"
+            }else{
+                imgWrapper.innerHTML = "";
+            }
+            hidePicDeleteMessage()
+        }
+
+        function showPicDeleteMessage(){
+            document.getElementById("body-imgs-upload-label-text").innerText = "Clique para remover a imagem"
+        }
+
+        function hidePicDeleteMessage(){
+            document.getElementById("body-imgs-upload-label-text").innerText = "Imagens do corpo do projeto"
+        }
+
+        let fileInput = document.getElementById("file-upload");
+        fileInput.addEventListener('change', () => {
+            if(fileInput.files.length > 0) {
+                document.getElementById('project-file-upload-label').innerHTML = `<div id="project-file-upload-label-text">Arquivo do projeto</div><div id="file-preview" onmouseover="showFileDeleteMessage()" onmouseout="hideFileDeleteMessage()"><img class="file-icon-create-project" src="/img/file-icon.png">${fileInput.files[0].name}</div>`
+            }
+            let filePreview = document.getElementById('file-preview');
+            filePreview.addEventListener('click', () => {
+                event.preventDefault();
+                let dataTransfer = new DataTransfer();
+                fileInput.files = dataTransfer.files;
+                document.getElementById('project-file-upload-label').innerHTML = `Arquivo do projeto`
+            })
+        })
+
+        function showFileDeleteMessage(){
+            document.getElementById("project-file-upload-label-text").innerText = "Clique para remover o arquivo"
+        }
+
+        function hideFileDeleteMessage(){
+            document.getElementById("project-file-upload-label-text").innerText = "Arquivo do projeto"
+        }
+
     </script>
 </body>
 
