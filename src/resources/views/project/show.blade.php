7b8a0140e021ab3c40df3d00105f938c1b0a003a
Flavio Santos <flawyo2002@gmail.com>
2024-08-23 18:01:14 -0300

    chore: atualiza versão do Bootstrap para 5.3.3

--- src/resources/views/project/show.blade.php
+++ src/resources/views/project/show.blade.php
@@ -9,8 +9,8 @@
     <title>Grafic Hub</title>
     <link href="/img/logo.png" rel ="icon">
     <link href="/css/projectfullview.css" rel ="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
 <header>
--- src/resources/views/user/perfil.blade.php
+++ src/resources/views/user/perfil.blade.php
@@ -12,7 +12,7 @@
     <link href="/css/navbar.css" rel ="stylesheet">
     <link href="/css/perfil.css" rel ="stylesheet">
     <link href="/css/project.css" rel ="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
 
