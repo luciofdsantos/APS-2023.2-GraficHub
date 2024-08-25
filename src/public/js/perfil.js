9c7ec33920f91bf0ffb26dda8bd454504b5a988b
Filinho <ivanderleimendessilvafilho@gmail.com>
2024-08-25 14:01:19 -0300

    style: modal edit profile

--- src/public/js/perfil.js
+++ src/public/js/perfil.js
@@ -1,3 +1,7 @@
 document.getElementById("btn-create-project").onclick = function (){
     document.getElementById("box-create-project").showModal();
 }
+
+document.getElementById("btn-edit-profile").onclick = function (){
+    document.getElementById("box-edit-profile").showModal();
+}
--- src/resources/views/user/perfil.blade.php
+++ src/resources/views/user/perfil.blade.php
@@ -85,8 +85,7 @@
 
         <div class="mainperfil options">
         </div>
-
-        <a class="edit mainshadowdown" href="{{ route('user.edit', $user['apelido']) }}" >Editar Perfil</a>
+        <button id = "btn-edit-profile" class="edit mainshadowdown">Editar Perfil</button>
         <a class="logout mainshadowdown" onclick="confirmLogout(event)" href ="{{route('auth.logout')}}">LogOut</a>
     </div>
     <div class="portifolio mainperfil mainshadowdown">
@@ -280,8 +279,63 @@
     </script>
     </dialog>
 
+    <dialog id = "box-edit-profile">
+        <div class="user-form-container">
+            <div class="main">
+                <form class="forms" action="{{ route('user.update', $user['apelido']) }}" method="post" enctype="multipart/form-data">
+                    <a href="{{ route('home')}}"> <img class="logo-img" alt="logo" src="/img/logo.png"/> </a>
+                    <p class="title"> Atualizar Dados</p>
+                    @csrf
+                    @method('PUT')
+                    <input class="mainshadowdown" type="text" placeholder="nome" name="nome"
+                           value="{{ old('nome', $user['nome']) }}">
+                    @error('nome')
+                    <span class="error-message">
+                    {{ $message }}
+                </span>
+                    @enderror
+                    <input class="mainshadowdown" type="text" placeholder="apelido" name="apelido"
+                           value="{{ old('apelido', $user['apelido']) }}">
+                    @error('apelido')
+                    <span class="error-message">
+                    {{ $message }}
+                </span>
+                    @enderror
+                    <input class="mainshadowdown" type="text" placeholder="número de telefone" name="numero_telefone"
+                           value="{{ old('numero_telefone', $user['numero_telefone']) }}">
+                    @error('numero_telefone')
+                    <span class="error-message">
+                    {{ $message }}
+                </span>
+                    @enderror
+                    <input class="mainshadowdown" type="text" placeholder="email" name="email"
+                           value="{{ old('email', $user['email']) }}">
+                    @error('email')
+                    <span class="error-message">
+                    {{ $message }}
+                </span>
+                    @enderror
+                    <input class="mainshadowdown" type="password" placeholder="Nova senha" name="password">
+                    @error('password')
+                    <span class="error-message">
+                    {{ $message }}
+                </span>
+                    @enderror
+                    <input class="mainshadowdown" type="password" placeholder="confirme a nova senha" name="password_confirmation">
+                    <label class="mainshadowdown custom-file-upload" for="file-upload">Foto de Perfil</label>
+                    <input id="file-upload" type="file" name="foto" placeholder="Foto de Perfil">
+                    @error('foto')
+                    <span class="error-message">
+                    {{ $message }}
+                </span>
+                    @enderror
+                    <button class="mainshadowdown" type="submit">Atualizar</button>
+                    <p class="message"><a href="{{ route('user.perfil', $user['apelido']) }}">Voltar para o Perfil</a></p>
+                </form>
+            </div>
+        </div>
+    </dialog>
     </body>
-
 
 </html>
 
