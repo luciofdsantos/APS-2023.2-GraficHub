545aeb25d03ec9487d9cffa36e1a25b5439c0300
Filinho <ivanderleimendessilvafilho@gmail.com>
2024-08-27 20:15:52 -0300

    fix: home - concertando botão descobri/seguidos

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -53,7 +53,8 @@
     <div class="box-options mainhome">
     <div class="box-intern">
             <div id ="descobrir-box" class="options"><a  id="descobrir"  href="{{ route('home') }}" onclick="discBGcolor()">Descobrir</a></div>
-            <div id="seguindo-box" class="options" ><a   id="seguindo" href="{{ route('home.personalizado') }}" onclick="followBGcolor()">Seguindo</a></div>
+
+            <div id="seguindo-box" class="options" ><a   id="seguindo" href="{{ route('home.personalizado') }}" @if(auth()->check())onclick="followBGcolor()" @else  onclick="setOut()" @endif>Seguindo</a></div>
     </div>
     </div>
 
