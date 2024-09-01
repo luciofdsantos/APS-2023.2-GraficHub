bd3a504c4205ad1f95a338dd9c0fd7f908c50b27
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-09-01 00:00:33 -0300

    refactor: transformar form de cadastro em componente
    Formulario de cadastro de usuario agora é um componente usado em na view user/create

---
+++ src/resources/views/components/usuario/form-cadastro.blade.php
@@ -0,0 +1,52 @@
+<div  class="user-form-container">
+    <div class ="main">
+        <form class="forms" action="{{ route('user.store') }}" method="post" enctype="multipart/form-data">
+            <a href="{{ route('home')}}"> <img class="logo-img" alt="logo" src="/img/logo.png"/> </a>
+            <p class="title"> Cadastrar</p>
+            @csrf
+            <input class="mainshadowdown" type="text" placeholder="nome" name="nome" value="{{ old('nome') }}">
+            @error('nome')
+            <span class="error-message">
+                    {{ $message }}
+                </span>
+            @enderror
+            <input class="mainshadowdown" type="text" placeholder="apelido" name="apelido" value="{{ old('apelido') }}" >
+            @error('apelido')
+            <span class="error-message">
+                    {{ $message }}
+                </span>
+            @enderror
+            <input class="mainshadowdown" type="text" placeholder="número de telefone" name="numero_telefone" value="{{ old('numero_telefone') }}">
+            @error('numero_telefone')
+            <span class="error-message">
+                    {{ $message }}
+                </span>
+            @enderror
+            <input class="mainshadowdown" type="text" placeholder="email" name="email" value="{{ old('email') }}">
+            @error('email')
+            <span class="error-message">
+                    {{ $message }}
+                </span>
+            @enderror
+            <input class="mainshadowdown" type="password" placeholder="senha" name="password">
+            @error('password')
+            <span class="error-message">
+                    {{ $message }}
+                </span>
+            @enderror
+            @error('error')
+            <span class="error-message">{{ $message }}</span>
+            @enderror
+            <input  class="mainshadowdown" type="password" placeholder="confirme a senha" name="password_confirmation">
+            <label class="mainshadowdown  custom-file-upload" for="file-upload">Foto de Perfil</label>
+            <input id ="file-upload" type="file" placeholder="Foto de Perfil" name="foto" />
+            @error('imagem')
+            <span class="error-message">
+                    {{ $message }}
+                </span>
+            @enderror
+            <button  class="mainshadowdown" type="submit">Cadastrar</button>
+            <p class="message">Já possui conta?  <a href="{{ route('auth.loginForm') }}">Faça login</a></p>
+        </form>
+    </div>
+</div>
--- src/resources/views/user/create.blade.php
+++ src/resources/views/user/create.blade.php
@@ -1,8 +1,17 @@
-@extends('nonav')
+<!doctype html>
+<html lang="en">
 
-@section('content')
+<head>
 
-
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Grafic Hub</title>
+    <link href="/img/logo.png" rel ="icon">
+    <link href="/css/login.css" rel ="stylesheet">
+</head>
+<body>
     @if (session()->has('success'))
         {{ session()->get('sucess') }}
     @endif
@@ -10,58 +19,9 @@
     @if (auth()->check())
         Usuario logado, {{ auth()->user()->nome }} <a href="{{ route('auth.logout') }}"> Logout </a>
     @else
-
-        <div  class="user-form-container">
-            <div class ="main">
-            <form class="forms" action="{{ route('user.store') }}" method="post" enctype="multipart/form-data">
-                <a href="{{ route('home')}}"> <img class="logo-img" alt="logo" src="/img/logo.png"/> </a>
-                <p class="title"> Cadastrar</p>
-                @csrf
-                <input class="mainshadowdown" type="text" placeholder="nome" name="nome" value="{{ old('nome') }}">
-                @error('nome')
-                <span class="error-message">
-                    {{ $message }}
-                </span>
-                @enderror
-                <input class="mainshadowdown" type="text" placeholder="apelido" name="apelido" value="{{ old('apelido') }}" >
-                @error('apelido')
-                <span class="error-message">
-                    {{ $message }}
-                </span>
-                @enderror
-                <input class="mainshadowdown" type="text" placeholder="número de telefone" name="numero_telefone" value="{{ old('numero_telefone') }}">
-                @error('numero_telefone')
-                <span class="error-message">
-                    {{ $message }}
-                </span>
-                @enderror
-                <input class="mainshadowdown" type="text" placeholder="email" name="email" value="{{ old('email') }}">
-                @error('email')
-                <span class="error-message">
-                    {{ $message }}
-                </span>
-                @enderror
-                <input class="mainshadowdown" type="password" placeholder="senha" name="password">
-                @error('password')
-                <span class="error-message">
-                    {{ $message }}
-                </span>
-                @enderror
-                @error('error')
-                <span class="error-message">{{ $message }}</span>
-                @enderror
-                <input  class="mainshadowdown" type="password" placeholder="confirme a senha" name="password_confirmation">
-                <label class="mainshadowdown  custom-file-upload" for="file-upload">Foto de Perfil</label>
-                <input id ="file-upload" type="file" placeholder="Foto de Perfil" name="foto" />
-                @error('imagem')
-                <span class="error-message">
-                    {{ $message }}
-                </span>
-                @enderror
-                <button  class="mainshadowdown" type="submit">Cadastrar</button>
-                <p class="message">Já possui conta?  <a href="{{ route('auth.loginForm') }}">Faça login</a></p>
-            </form>
-            </div>
-        </div>
+        <x-form-cadastro />
     @endif
-@endsection
+
+</body>
+
+</html>
