5866f54f86bf71a3e4d6cb9b0d3144cd2786fd95
Pedro Dias <pgd@aluno.ifnmg.edu.br>
2024-09-01 15:35:34 -0300

    fix: corrige erro ao deletar projeto favoritado

--- src/database/migrations/2024_08_30_232228_create_favorites_table.php
+++ src/database/migrations/2024_08_30_232228_create_favorites_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('project_id')->constrained('projects');
+            $table->foreignId('project_id')->constrained('projects')->onDelete('CASCADE');
             $table->timestamps();
         });
     }
