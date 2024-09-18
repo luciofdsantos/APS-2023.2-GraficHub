d3324cd4d1e954d32b1d77be6400206084234c90
Pedro Dias <pgd@aluno.ifnmg.edu.br>
2024-09-18 10:43:15 -0300

    update: atualiza factory do user com os novos campos

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -24,9 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'nome' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'apelido' => fake()->unique()->userName,
+            'numero_telefone' => fake()->unique()->phoneNumber,
+            'foto' => null,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
